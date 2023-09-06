1c9cd39e48b1be8836b0829de59ff5a61f434d4e
Butawantemi <japhetbuta@gmail.com>
2023-09-06 23:42:22 +0300

    corrections

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for j in range(ord('a'), ord('z')+1):
-    if chr(j) not in ['q', 'e']:
-         print("{}".format(chr(j)), end="")     
+ if chr(j) not in ['q', 'e']:
+  print("{}".format(chr(j)), end="")     
