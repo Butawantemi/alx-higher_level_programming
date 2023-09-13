d9107c4367449ece0359f95b6ec42d79b2d44bd5
Butawantemi <japhetbuta@gmail.com>
2023-09-13 23:55:10 +0300

    10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary is None or a_dictionary == {}:
         return None
-    best_key = None
-    best_value = float('-inf') 
-    for key, value in a_dictionary.items():
-        if value > best_value:
-            best_key = key
-            best_value = value
-    return best_key
+    max_value = max(a_dictionary, key=lambda k: a_dictionary[k])
+    return max_value
