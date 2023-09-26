cbea6a76a41907dd0e5279ac81763769ec702e91
Butawantemi <japhetbuta@gmail.com>
2023-09-27 00:33:49 +0300

    0x06-python-classes

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,7 +5,4 @@ class Square:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size  # Private attribute with a double underscore prefix
-
-    def area(self):
-        return self.__size ** 2
+        self.__size = size
