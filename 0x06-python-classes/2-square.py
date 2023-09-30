1d161e0520f2473d5f71723008f4d1eaa2afb49a
Butawantemi <japhetbuta@gmail.com>
2023-09-30 12:48:39 +0300

    0x06-python-classes

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
+"""define a class square"""        
 class Square:
+  
+    """initilize new square with private instance attribute"""
     def __init__(self, size=0):
         if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
+            raise TypeError("size must be an integer")  
+        if size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size  
-    def area(self):
-        return self.__size ** 2
+        self.__size = size
