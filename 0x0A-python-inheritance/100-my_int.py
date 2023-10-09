c376cf8c0106d6e5c64b3ba27db5aba5c96dc513
Butawantemi <japhetbuta@gmail.com>
2023-10-09 17:42:07 +0300

    0x0A-python-inheritance

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """Write a class MyInt that inherits from int"""
 class MyInt(int):
+    """A subclass of class int"""
     def __eq__(self, other):
+        """sets the behaviour of == """
         return super().__ne__(other)
-
+    """sets the != behavior"""
     def __ne__(self, other):
         return super().__eq__(other)
