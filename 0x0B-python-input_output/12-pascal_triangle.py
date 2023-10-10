4bb7d7108e97e888c76b79cdfc51dfe4864d7688
Butawantemi <japhetbuta@gmail.com>
2023-10-10 20:39:50 +0300

    0x0B-python-input_output

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Generate Pascal's triangle up to the nth row"""
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = []
+
+    for i in range(n):
+        row = []
+
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                row.append(1)
+            else:
+                value = triangle[i - 1][j - 1] + triangle[i - 1][j]
+                row.append(value)
+
+        triangle.append(row)
+
+    return triangle
