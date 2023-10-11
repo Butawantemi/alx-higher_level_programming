4d1b19cf43a413c0201d53cf74149e21c1e8e48f
Butawantemi <japhetbuta@gmail.com>
2023-10-11 23:29:52 +0300

    0x0B-python-input_output

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Insert a line of text after each line containing a specific string in a file"""
 def append_after(filename="", search_string="", new_string=""):
+    """a function that inserts a line of text to a file"""
     with open(filename, "r") as f:
         text = f.readlines()
 
