6ad3ab758398f25762c3554ba3a737128284f2e2
Butawantemi <japhetbuta@gmail.com>
2023-10-11 23:05:37 +0300

    0x0A-python-inheritance

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,1 +1,20 @@
-cnnvcvvjj
+#!/usr/bin/python3
+"""The module"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """A subclass of BaseGeometry class"""
+    def __init__(self, width, height):
+        """initialize private attributes"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+    def area(self):
+        """returns area of a rectangle and overrides area()"""
+        return self.__width * self.__height
+    
+    def __str__(self):
+        """Returns string representation"""
+        return str("[Rectangle] {}/{}".format(self.__width, self.__height))
