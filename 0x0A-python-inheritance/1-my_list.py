9dec88e28316e72a290f7a83ed6f1cd3634f7fca
Butawantemi <japhetbuta@gmail.com>
2023-10-11 22:35:40 +0300

    0x0A-python-inheritance

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,2 +1,12 @@
+#!/usr/bin/python3
 
-ooooo
+"""The module"""
+class MyList(list):
+    """A subclass of list"""
+    def __init__(self):
+        """Initialize the object"""
+        super().__init__()
+
+    def print_sorted(self):
+        """Prints the sorted list"""
+        print(sorted(self))
