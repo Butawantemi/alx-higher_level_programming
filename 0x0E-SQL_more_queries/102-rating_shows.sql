9e8f44495f0fe86e8948fbdecbea3bf514f3c993
Butawantemi <japhetbuta@gmail.com>
2023-11-15 20:31:00 +0300

    0x0E-SQL_more_queries

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -3,8 +3,7 @@
 -- Results must be sorted in descending order by the rating
 -- You can use only one SELECT statement
 
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows LEFT JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_show_genres.genre_id IS NULL
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+SELECT title, SUM(rating) AS rating_sum
+FROM hbtn_0d_tvshows_rate.shows
+GROUP BY title
+ORDER BY rating_sum DESC;
