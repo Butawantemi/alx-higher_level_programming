1150ac6d3574f176729c8a9700267eb352535740
Butawantemi <japhetbuta@gmail.com>
2023-12-17 13:28:21 +0300

    0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,29 +1,31 @@
 #!/usr/bin/python3
 """
-Module that connects a python script to a database
+script that lists all states from the database hbtn_0e_0_usa
 """
 
 if __name__ == "__main__":
-
+    
     import MySQLdb
     from sys import argv
-
+    
+    #connect db using command-line argurments
     my_db = MySQLdb.connect(host='localhost', user=argv[1], password=argv[2],
                             db=argv[3], port=3306)
-    
+    #obj to interact with database
     my_cursor = my_db.cursor()
 
-    
+    #query to fetch data
     my_cursor.execute("SELECT * FROM states ORDER BY states.id ASC;")
 
-    
+    #data returned by the query
     my_data = my_cursor.fetchall()
 
-    
+    #fetched data and print each row
     for row in my_data:
         print(row)
 
-    
+    #close cursor
     my_cursor.close()
-
+    
+    #close db
     my_db.close()
