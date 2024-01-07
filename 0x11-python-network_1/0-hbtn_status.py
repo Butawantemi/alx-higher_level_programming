f9305c8fc8672820cb701fbcd005aec6e41dbe2a
Butawantemi <japhetbuta@gmail.com>
2024-01-07 21:40:47 +0300

    python network

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python3
-""" using urllib to fetch https://alx-intranet.hbtn.io/status """
+""" Script that fetches https://alx-intranet.hbtn.io/status """
 
 from urllib import request
 
 if __name__ == "__main__":
     url = 'https://alx-intranet.hbtn.io/status'
-
-    # fetch the URL
     with request.urlopen(url) as response:
-        # Read and decode the response body
-        response_body = response.read()
-
-        # Display the response body with tabulation
+        content = response.read()
         print("Body response:")
-        print("\t- type: {}".format(type(response_body)))
-        print("\t- content: {}".format(response_body))
-        print("\t- utf8 content: {}".format(response_body.decode('utf-8')))
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
