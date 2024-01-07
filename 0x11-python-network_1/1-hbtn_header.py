472f9760f6fb640da8d992ad05ea261a157e87a8
Butawantemi <japhetbuta@gmail.com>
2024-01-07 21:46:02 +0300

    python network

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,6 +6,8 @@ import sys
 from urllib import request
 
 if __name__ == "__main__":
+
+    # store the URL from shell argument.
     url = sys.argv[1]
     with request.urlopen(url) as response:
         print(dict(response.headers).get("X-Request-Id"))
